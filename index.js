fccd7c817a0d976af1872092993b420bcf964b54
Rox <jagaaoguz.7000@gmail.com>
2025-04-08 14:11:48 +0300

    js added

---
+++ index.js
@@ -0,0 +1,21 @@
+const input = document.getElementById("num");
+const label = document.getElementById("labl");
+const h3 = document.querySelector("h3");
+
+label.addEventListener("click", () => {
+  const validSsn = (value) =>
+    value &&
+    /^(?!(000|666|9))(\d{3}-?(?!(00))\d{2}-?(?!(0000))\d{4})$/.test(value)
+      ? true
+      : false;
+
+  //=============
+  const output = validSsn(input.value);
+  if (output) {
+    h3.innerText = `Entered SSN (${input.value}) is valid`;
+    input.value = "";
+  } else {
+    h3.innerText = `SSN must be 11 character (9 numbers and two hypens)`;
+    input.value = "";
+  }
+});
